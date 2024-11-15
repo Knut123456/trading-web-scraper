6581a683e66a65b0b3731554725c060de9b96b2a
Knut123456 <knut.rote@gmail.com>
2024-11-15 11:21:58 +0100

    fått directory til å fungere

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # trading-web-scraper-and-ai
 
+## velkommen til mitt proskjekt 
 
-velkommen til mitt proskjekt 
-
-jeg skal lage en trading bot med bruk av api og en ai til å få det til å vise hva jeg burde investere 
+jeg skal lage en trading bot med bruk av api eller web scraper og en ai til å få det til å vise hva jeg burde investere. vil også gjøre slik 
--- python.py
+++ python.py
@@ -1,6 +1,8 @@
 import json
 from bs4 import BeautifulSoup
 import requests
+import time
+import random
 
 json_file = "info.json"
 url = "https://www.nordnet.no/market/stocks?selectedTab=overview&exchangeCountry=NO"
@@ -10,46 +12,71 @@ page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 
 #print(soup.prettify())
-
+spandict={}
 
 
 
 
 names = []
+
 # Find the table with the stock information
 
 div = soup.find_all("div", role="table")
-rows = soup.find_all( role="row")
-data_list = ()
+rows = soup.find_all("div", role="row")
 
-data_lists = {
-    
-}
 thisdict = {
-    "row":  "{}"
+    
 }
 #print(rows[0].get_text().strip())
 
 
 rows0 = rows[0]
-rows0span = rows0.find_all("span")
+rows0pan = rows0.find_all("span")
 
 
-for span in rows0span:
-        span_text = span.get_text().strip()
-        names.append(span_text)
+#rows1 = rows[1]
+#rows1_div = rows1.find_all("div")
+#rows1pan = rows1_div.find(text=True)
+
+
+for text in rows0pan:
+    row0_text = rows0pan.get_text().strip()
+    names.append(row0_text)
+
+
+print (names)
 #for span in rows0span:
      #rows0span0 = span[0].get_text().strip()
      #print(span.get_text().strip())
-
+rowcheck = True
 for row in rows:
-    i = 0
+    #time.sleep(random.randint(1,3))
     # Find all span elements in each row
-    spans = row.find_all("span")
+    divs = row.find_all("div")
+    texts = div.find_all(text = True)
     
+    rowdict = {}
+    if rowcheck == True:
+        print("row is working")
+    rowcheck = False
     # Extract and print or store the text content from each span
-    for span in spans:
-          
+    for div in divs:
+        for text in texts:
+            for index, value in enumerate(texts):
+                name = names[index]
+                rowdict[name] = span_text
+            
+                span_text = span.get_text().strip()
+                    
+
+                    #print(f"Index: {index}, Value: {value}")
+                    #print ("for span loop is working")
+                
+
+    thisdict["row1"] = rowdict
+    
+
+print(thisdict)        
 
           
        
