36418ea29aacbfff899aa487f4f0761ed6db26f2
Knut123456 <knut.rote@gmail.com>
2024-11-29 11:27:41 +0100

    jobba videre med å få det til å fungere

--- python.py
+++ python.py
@@ -1,29 +1,49 @@
-import json
+
 from bs4 import BeautifulSoup
 import requests
-import time
-import random
+import pandas as pd
 
+
+pd.set_option('display.max_rows', None)
+info_dict = {
+    
+}
 json_file = "info.json"
 url = "https://www.nordnet.no/market/stocks?selectedTab=overview&exchangeCountry=NO"
 
-page = requests.get(url)
+page = requests.get(url, verify=False)
 
 soup = BeautifulSoup(page.text, "html.parser")
 
 names = []
 
-divs = soup.find("div", role = "table")
-divs_len = len(divs)
+glem_index =[]
 
-print(divs_len)
+index = -1
+divs = soup.find("div", role = "table")
 for div in divs:
-    div0 = div[0]
-    if div == div0:
-     for row in div:
-        names.append(row)
-    #print(div)
-    div_text =div.get_text().strip()
-    #print(div_text)
-   
-print(names)
+    text_index = -1
+    name_index = -1
+    index += 1
+    row_dict ={}
+    if index == 0 :
+        for name in div:
+            name_index += 1
+            name_text = name.get_text().strip()
+            names.append(name_text)
+    #print(names)            
+                 
+            
+    if index != 0 :
+        for text in div:
+            text_index += 1
+            only_text=text.get_text().strip()
+            info_dict[names[text_index]] = only_text
+            
+
+
+    
+    #print(index)
+#print(glem_index)
+#print(names)
+print(info_dict)
